1e880d7a327a5350defa85704db7d216b605417c
Diego Rios <diegoarv08@hotmail.com>
2025-02-24 00:14:47 -0400

    Convertir cualquier numero

--- src/num_romanos.js
+++ src/num_romanos.js
@@ -2,14 +2,17 @@ const valores =[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 const simbolos = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
 function convertir_a_romanos(numero) {
-  var resultado = "";
+  var resultado ="";
   
   if(numero<=0)
     resultado="0"
   else if( numero>=1 && numero<=5000)
   {
     for (let i = 0; i < valores.length; i++){
-
+      while (numero >= valores[i]) {
+        resultado += simbolos[i];
+        numero -= valores[i];
+    }
     }
   }
   return resultado;
--- src/num_romanos.test.js
+++ src/num_romanos.test.js
@@ -1,34 +1,39 @@
 import convertir_a_romanos from "./num_romanos.js";
 
 describe("Convertir 1 a I", () => {
-  it("deberia convertir uno en romano", () => {
+  it("deberia convertir 1 en romano", () => {
     expect(convertir_a_romanos(1)).toEqual("I");
   });
 });
 
 describe("Convertir 5 a I", () => {
-  it("deberia convertir uno en romano", () => {
+  it("deberia convertir 5 en romano", () => {
     expect(convertir_a_romanos(5)).toEqual("V");
   });
 });
 
 describe("Convertir 9 a I", () => {
-  it("deberia convertir uno en romano", () => {
+  it("deberia convertir 9 en romano", () => {
     expect(convertir_a_romanos(9)).toEqual("IX");
   });
 });
 
 describe("No convertir a 0", () => {
-  it("deberia convertir uno en romano", () => {
+  it("deberia convertir 0 en 0", () => {
     expect(convertir_a_romanos(0)).toEqual("0");
   });
 });
 
 describe("No convertir numeros menores a 0", () => {
-  it("deberia convertir uno en romano", () => {
+  it("No deberia convertir dandonos 0", () => {
     expect(convertir_a_romanos(-15)).toEqual("0");
   });
 });
 
+describe("Convertir 2584 a romano", () => {
+  it("deberia convertir 2584 en romano", () => {
+    expect(convertir_a_romanos(-15)).toEqual("MMDLXXXIV");
+  });
+});
 
 
